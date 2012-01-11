c390f5b6c530fca3c089e3d51e2b4ce9a0c53bb3
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2012-01-11 12:17:09 +0200

    first_staps

--- dropline/views.py
+++ dropline/views.py
@@ -1,5 +1,26 @@
 from pyramid.view import view_config
+from pyramid.renderers import render_to_response
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project':'dropline'}
+
+
+@view_config(route_name='uploader')
+def upload_view(request, template_name = 'templates/index.pt'):
+    list_of_files = None
+
+    if request.method == 'POST':
+        post_body = request.POST
+        
+        template_name = 'templates/complete.pt'
+#        import pdb; pdb.set_trace()
+        
+        file_name = request.POST['file'].filename
+        return render_to_response(template_name, {  'post_body': post_body,
+                                                    'file_name': file_name,
+                                                    }, request=request)
+
+
+    return render_to_response(template_name, {'project':'dropline', 'list_of_files': list_of_files}, request=request)
+
