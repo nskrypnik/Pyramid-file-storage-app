92775a06cd680125239f155c80fc3043ff779da3
Ivanchenko Dmitry <ivanchenko.dimitry@gmail.com>
2012-01-12 16:15:37 +0200

    some distribution

--- dropline/__init__.py
+++ dropline/__init__.py
@@ -6,6 +6,7 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('index', '/index')
     config.add_route('uploader', '/uploader')
     config.scan()
     return config.make_wsgi_app()
--- dropline/views.py
+++ dropline/views.py
@@ -6,21 +6,24 @@ def my_view(request):
     return {'project':'dropline'}
 
 
-@view_config(route_name='uploader')
-def upload_view(request, template_name = 'templates/index.pt'):
+@view_config(route_name='index')
+def index_view(request, template_name = 'templates/index.pt'):
     list_of_files = None
+
+    return render_to_response(template_name, {'project':'dropline', 'list_of_files': list_of_files}, request=request)
+
+
+@view_config(route_name='uploader')
+def upload_view(request, template_name = 'templates/complete.pt'):
 
     if request.method == 'POST':
         post_body = request.POST
-        
-        template_name = 'templates/complete.pt'
-#        import pdb; pdb.set_trace()
-        
         file_name = request.POST['file'].filename
         return render_to_response(template_name, {  'post_body': post_body,
                                                     'file_name': file_name,
                                                     }, request=request)
 
+    return render_to_response(template_name, {}, request=request)
 
-    return render_to_response(template_name, {'project':'dropline', 'list_of_files': list_of_files}, request=request)
+
 
